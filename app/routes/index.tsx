964cb0aa3f29d8442c09939f82a5b552f3c03def
Zakiego <belummikir@gmail.com>
2022-01-03 00:05:07 +0800

    feat: index html

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,17 +1,26 @@
 export default function Index() {
   return (
-    <div className="bg-black flex min-h-screen">
-      <div className="m-auto space-y-3">
-        <h1 className="text-white/90 text-4xl">
+    <div className="flex min-h-screen bg-black">
+      <div className="m-auto space-y-3 text-center text-white/90">
+        <h1 className="text-xl md:text-4xl">
           Welcome to{" "}
           <a
-            className="text-blue-400 bg-gray-800/95 rounded-md px-2 py-1 hover:bg-gray-800/80 transition-all ease-in-out duration-300"
+            className="px-2 py-1 font-semibold text-blue-400 transition-all duration-300 ease-in-out rounded-md bg-gray-800/95 hover:bg-gray-800/80"
             href="https://remix.run/"
           >
             Remix Run
           </a>
         </h1>
-        <div className="rounded-md px-2 py-1 text-white/90  hover:bg-gray-800/80 transition duration-700 ease-in-out">
+        <h2 className="text-lg md:text-2xl">
+          With{" "}
+          <a
+            className="px-2 py-1 text-blue-400 transition-all duration-300 ease-in-out rounded-md bg-gray-800/95 hover:bg-gray-800/80"
+            href="https://tailwindcss.com/"
+          >
+            Tailwind CSS
+          </a>
+        </h2>
+        <div className="px-2 py-1 text-xs transition duration-700 ease-in-out rounded-md md:text-base hover:bg-gray-800/80">
           <a href="https://github.com/zakiego/remix-tailwindcss-starter">
             https://github.com/zakiego/remix-tailwindcss-starter
           </a>

27a4547314e5fec22228740ace58ded92a49cf80
Zakiego <belummikir@gmail.com>
2022-01-04 21:03:37 +0800

    feat: tailwind add custom colors and boxShadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black-smooth": "#111827",
+        "black-fade": "#6B7280",
+      },
+      boxShadow: {
+        card: "-10px 48px 140px 0px rgba(126,123,160,0.2)",
+      },
+    },
   },
   variants: {},
   plugins: [],
